26198058cf373bc65f605b566d022576b9a0ab52
SabineWren <sabinewrensc@gmail.com>
2018-03-10 16:15:15 -0800

    started adding org listings

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,9 +1,28 @@
+let success = function(r){
+	return { success: true, data: r.json() };
+};
+
+let fail = function(err){
+	return { success: false, error: err };
+};
+
 let fetchSizeHistory = async function(sid){
 	return await fetch("/backEnd/org_history.php?SID=" + sid)
-		.then(function(r){
-			return { success: true, data: r.json() };
-		})
-		.catch(function(err){
-			return { success: false, error: err };
-		});
+		.then(r => success(r))
+		.catch(err => fail(err));
+};
+
+let fetchOrgsListing = async function(){
+	return await fetch("/backEnd/selects.php?Activity=&Archetype=&Cog=0&Commitment=&Growth=down&Lang=Any&Manifesto=&NameOrSID=&OPPF=0&Recruiting=&Reddit=0&Roleplay=&STAR=0&pagenum=0&primary=0")
+		.then(r => success(r))
+		.catch(err => fail(err));
+};
+
+let parseResponse = function(response){
+	let data = response.data;
+	if(!response.success) {
+		data = [];
+		console.log("request failed: " + response.error);
+	}
+	return data;
 };
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -2,30 +2,77 @@
 
 window.onload = () => init();
 
-let createTab = function(orgName){
-	let newTab = document.createElement("div");
-	newTab.classList.add("tab");
-	newTab.innerHTML = orgName;
-	return newTab;
+let addCell = function(text){
+	let td = document.createElement("td");
+	td.innerHTML = text;
+	this.appendChild(td);
+	return this;
 };
 
-let createChart = async function(chartContainer, orgSID){
+let addChart = async function(chartContainer, orgSID){
 	let response = await fetchSizeHistory(orgSID);
-	let data = await response.data;
-	if(!response.success) {
-		data = [];
-		console.log("request failed: " + response.error);
-	}
+	let data = await parseResponse(response);
 	
 	let newChart = drawChartLine(chartContainer, data, orgSID);
 	newChart.classList.add("chart");
 	return chartContainer;
 };
 
+let addResults = async function(){
+	let tab = createTab("Default Listing");
+	document.getElementById("tab-holder-results").appendChild(tab);
+	
+	let resultsContainer = createResultsContainer();
+	document.getElementById("chart-holder").appendChild(resultsContainer);
+	
+	let response = await fetchOrgsListing();
+	let data = await parseResponse(response);
+	
+	loadList(resultsContainer, data);
+};
+
+let addRow = function(data){
+	let row = document.createElement("tr");
+	
+	row.addCell = addCell;
+	
+	row.addCell(data.Archetype)
+		.addCell(data.Commitment)
+		.addCell(data.GrowthRate)
+		.addCell(data.Language)
+		.addCell(data.Main)
+		.addCell(data.Name)
+		.addCell(data.PrimaryFocus)
+		.addCell(data.SID)
+		.addCell(data.SecondaryFocus)
+		.addCell(data.Size);
+	
+	this.appendChild(row);
+};
+
+let addTabAndChart = async function (orgSID, orgName) {
+	let tab = createTab(orgName);
+	document.getElementById("tab-holder-charts").appendChild(tab);
+	
+	let chartContainer = createChartContainer();
+	document.getElementById("chart-holder").appendChild(chartContainer);
+	
+	//the container MUST first be loaded in the DOM for its size to be non-zero
+	await addChart(chartContainer, orgSID);
+	
+	let closeIcon = createCloseIcon(tab, chartContainer);
+	tab.appendChild(closeIcon);
+};
+
 let createChartContainer = function(){
 	let chartContainer = document.createElement("div");
 	chartContainer.classList.add("chart-container");
 	return chartContainer;
+};
+
+let createResultsContainer = function(){
+	let resultsContainer = document.createElement("table");
+	return resultsContainer;
 };
 
 let createCloseIcon = function(keyElement, valueElement){
@@ -35,29 +82,15 @@ let createCloseIcon = function(keyElement, valueElement){
 	return closeIcon;
 }
 
-let onclickFactoryClose = function(keyElement, valueElement){
-	return function(){
-		keyElement.remove();
-		valueElement.remove();
-	};
-	
-};
-
-let addTabAndChart = async function (orgSID, orgName) {
-	let tab = createTab(orgName);
-	document.getElementById("tab-holder").appendChild(tab);
-	
-	let chartContainer = createChartContainer();
-	document.getElementById("chart-holder").appendChild(chartContainer);
-	
-	//the container MUST first be loaded in the DOM for its size to be non-zero
-	await createChart(chartContainer, orgSID);
-	
-	let closeIcon = createCloseIcon(tab, chartContainer);
-	tab.appendChild(closeIcon);
+let createTab = function(orgName){
+	let newTab = document.createElement("div");
+	newTab.classList.add("tab");
+	newTab.innerHTML = orgName;
+	return newTab;
 };
 
 let init = function () {
+	addResults();
 	addTabAndChart("LAWBINDERS","LAWBINDERS");
 	addTabAndChart("00000000", "ENEMY CONTACT");
 	addTabAndChart("HHCORP", "Horizons Hunters");
@@ -66,5 +99,32 @@ let init = function () {
 	addTabAndChart("TFTO", "The First Order");
 	addTabAndChart("PROT", "Protectors of Verum");
 	addTabAndChart("AMFR", "AMFR");
+};
+
+let loadList = function(resultsContainer, data){
+	resultsContainer.addRow = addRow;
+	
+	resultsContainer.addRow({
+		Archetype: "Archetype",
+		Commitment: "Commitment",
+		GrowthRate: "GrowthRate",
+		Language: "Language",
+		Main: "Main",
+		Name: "Name",
+		PrimaryFocus: "PrimaryFocus",
+		SID: "SID",
+		SecondaryFocus: "SecondaryFocus",
+		Size: "Size",
+	});
+	
+	data.forEach(dataRow => resultsContainer.addRow(dataRow));
+};
+
+let onclickFactoryClose = function(keyElement, valueElement){
+	return function(){
+		keyElement.remove();
+		valueElement.remove();
+	};
+	
 };
 
